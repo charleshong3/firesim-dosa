989eb9746f473c0e6a398876e4c7c66108944b58
Cloud User <centos@ip-192-168-0-225.ec2.internal>
2023-08-08 08:50:00 +0000

    dosa artifact

---
+++ deploy/gemmini.py
@@ -0,0 +1,34 @@
+import subprocess
+import yaml
+
+#subprocess.run(["firesim", "launchrunfarm"])
+workloads = []
+target_configs = []
+# for i in range(773):
+#     workloads.append(f"gemmini_{i}.json")
+# for i in [4, 8, 16, 32, 64, 128]:
+#     for t in ["matmul", "conv"]:
+#         workloads.append(f"gemmini_{i}pe_{t}.json")
+#         target_configs.append(f"firesim_rocket_singlecore_{i}pe4kdummygemmini_nofirstlayer_no_nic_l2_llc4mb_ddr3")
+# for i in [2]:
+#     for t in ["conv"]:
+#         workloads.append(f"gemmini_{i}pe_{t}.json")
+#         target_configs.append(f"firesim_rocket_singlecore_{i}pe4kdummygemmini_nofirstlayer_no_nic_l2_llc4mb_ddr3")
+workloads = ["gemmini_matmul.json", "gemmini_conv.json"]
+target_configs.append("firesim_rocket_singlecore_16pe4kdummygemmini_nofirstlayer_no_nic_l2_llc4mb_ddr3")
+target_configs.append(target_configs[0])
+
+for i in range(len(workloads)):
+    config_runtime_path = "/home/centos/firesim/deploy/config_runtime.yaml"
+    with open(config_runtime_path, "r") as f:
+        config = yaml.safe_load(f)
+    config["workload"]["workload_name"] = workloads[i]
+    config["target_config"]["default_hw_config"] = target_configs[i]
+    with open(config_runtime_path, "w") as f:
+        yaml.dump(config, f)
+    subprocess.run(["firesim", "infrasetup"])
+    subprocess.run(["firesim", "runworkload"])
+
+# p = subprocess.Popen(["firesim", "terminaterunfarm"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+# p.communicate("yes\n".encode())
+
